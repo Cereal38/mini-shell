717c0b574acdeff0d29861839e64607f37911091
Mohamed Amarcha <simoamarcha@icloud.com>
2024-02-13 15:22:23 +0100

    Handle external commands

--- src/shell.c
+++ src/shell.c
@@ -29,23 +29,22 @@ int main()
 			continue;
 		}
 
-		
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-
-		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			printf("seq[%d]: ", i);
-			for (j=0; cmd[j]!=0; j++) {
-				printf("%s ", cmd[j]);
-			}
-			printf("\n");
-		}
 		if (strcmp(l->seq[0][0], "quit") == 0)
 		{
 			exit(0);
 		}
+		// All external commands are processed here
+		else {
+			// We create a child to run the external cmd
+			if (Fork() == 0) { // Child
+				execvp(l->seq[0][0],l->seq[0][1]);
+			}
+
+			else { // Parent
+				Wait(NULL);
+			}
+		}
+		
 		
 	}
 
