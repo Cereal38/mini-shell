970ef4667817ba9392780c02debcded2728fd1e4
Mohamed Amarcha <simoamarcha@icloud.com>
2024-02-16 15:23:49 +0100

    Pipe is working

--- src/utils.c
+++ src/utils.c
@@ -55,9 +55,48 @@ void exec_internal(struct cmdline *l){
 }
 
 void exec_external(struct cmdline *l){
+
   char *commande = l->seq[0][0];
+  // pipeFd[1] = STDIN
+  // pipeFd[0] = STDOUT
+  // Children with pipe
+  if(l->seq[1]){
+    char *commande2 = l->seq[1][0];
+    
+    int p[2];
+    pipe(p);
+    pid_t childPid1;
+    if ((childPid1 = Fork()) == 0) { // fils
+            Close(p[0]); 
+            
+            redirectInOut(l);
+            Dup2(p[1], STDOUT);
+            execvp(commande,l->seq[0]);
+            gestion_erreur(commande);
+            
+            Close(p[1]); 
+            exit(0);
+        } else { // père
+
+            pid_t childPid2 ;
+            if ((childPid2 = Fork())== 0) //fils 2
+            {
+                Close(p[1]); 
+                Dup2(p[0], STDIN);
+                redirectInOut(l);
+                execvp(commande2,l->seq[1]);
+                gestion_erreur(commande2);
+
+                Close(p[0]);
+                exit(0);
+            }
+            Close(p[0]);
+            Close(p[1]);
+            while (wait(NULL) > 0);
+  }}
   
-  if(Fork()==0){
+  // No pipe
+  else if(Fork()==0){
     // Redirect input and output
     redirectInOut(l);
     execvp(commande,l->seq[0]);
@@ -66,8 +105,5 @@ void exec_external(struct cmdline *l){
   } else {
     Wait(NULL);
   }
-
 }
-
-
 		
